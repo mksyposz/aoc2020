53b09fd0b94a50e969c621606671ef1a314c4e0e
Michal <mike@mikepad>
2020-12-08 18:45:08 +0100

    Change solution for part2 to O(n)

--- src/day08.rs
+++ src/day08.rs
@@ -1,8 +1,8 @@
 #[derive(Copy, Clone)]
 enum Command {
-    Nop {val: i32},
-    Acc {inc: i32},
-    Jmp {dist: i32},
+    Nop (i32),
+    Acc (i32),
+    Jmp (i32),
 }
 
 pub fn run<R>(input: R) -> (String, String)
@@ -15,15 +15,15 @@ where
         if line.is_empty() {continue;}
         let command = line.split_ascii_whitespace().collect::<Vec<&str>>();
         let c = match command[0] {
-            "nop" => {Command::Nop{val: command[1].parse().unwrap()}},
-            "acc" => {Command::Acc{inc: command[1].parse().unwrap()}},
-            "jmp" => {Command::Jmp{dist: command[1].parse().unwrap()}},
+            "nop" => Command::Nop(command[1].parse().unwrap()),
+            "acc" => Command::Acc(command[1].parse().unwrap()),
+            "jmp" => Command::Jmp(command[1].parse().unwrap()),
             _ => panic!("No such command"),
         };
         commands.push(c);
     }
     let ans1 = part_one(&commands);
-    let ans2 = part_two(&mut commands);
+    let ans2 = part_two_speedy(&mut commands);
     (ans1, ans2)
 }
 
@@ -46,9 +46,9 @@ fn loops(input: &Vec<Command>) -> (bool, i32) {
         }
         history[data.pos] = true;
         match input[data.pos] {
-            Command::Nop{val: _} => data.pos += 1,
-            Command::Acc{inc} => {data.acc += inc; data.pos += 1},
-            Command::Jmp{dist} => data.pos = ((data.pos as i32) + dist) as usize
+            Command::Nop(_) => data.pos += 1,
+            Command::Acc(inc) => {data.acc += inc; data.pos += 1},
+            Command::Jmp(dist) => data.pos = ((data.pos as i32) + dist) as usize
         }
     }
     (looped, data.acc)
@@ -59,26 +59,59 @@ fn part_one(input: &Vec<Command>) -> String {
     res.1.to_string()
 }
 
-fn part_two(input: &mut Vec<Command>) -> String {
-    let mut result = -1;
-    for i in 0..input.len() {
-        let original_command = input[i];
-        let res = match input[i] {
-            Command::Nop{val} => {
-                input[i] = Command::Jmp{dist: val};
-                loops(&input)
-            },
-            Command::Jmp{dist} => {
-                input[i] = Command::Nop{val: dist};
-                loops(&input)
-            }
-            _ => continue,
-        };
-        if !res.0 {
-            result = res.1;
+fn dfs(pos: usize, graph: &Vec<Command>, vis: &mut Vec<bool>, can_reach_end: &mut Vec<bool>) {
+    if pos >= graph.len() {can_reach_end[pos] = true; return;}
+    if vis[pos] {
+        return;
+    }
+    vis[pos] = true;
+    let next = match graph[pos] {
+        Command::Acc(_) | Command::Nop(_) => pos + 1,
+        Command::Jmp(dist) => ((pos as i32) + dist) as usize,
+    };
+    if next < graph.len() {
+        dfs(next, graph, vis, can_reach_end);
+        if can_reach_end[next] {
+            can_reach_end[pos] = true;
+        }
+    } else {
+        can_reach_end[pos] = true;
+    }
+}
+
+fn next_pos(j: i32, curr_pos: usize) -> usize {
+    ((curr_pos as i32) + j) as usize
+}
+
+fn part_two_speedy(input: &mut Vec<Command>) -> String {
+    let mut history: Vec<bool> = vec![false; input.len()];
+    let mut can_reach_end: Vec<bool> = vec![false; input.len()];
+    input.iter().enumerate().for_each(|(pos, _)| dfs(pos, &input, &mut history, &mut can_reach_end));
+    let mut data = Data{pos: 0, acc: 0};
+    let mut jumped = false;
+    loop {
+        if data.pos >= input.len() {
             break;
         }
-        input[i] = original_command;
+        match input[data.pos] {
+            Command::Nop(dist) => {
+                if !jumped && can_reach_end[next_pos(dist, data.pos)] {
+                    jumped = true;
+                    data.pos = next_pos(dist, data.pos);
+                } else {
+                    data.pos = data.pos + 1;
+                }
+            },
+            Command::Acc(inc) => {data.acc += inc; data.pos += 1},
+            Command::Jmp(dist) => {
+                if !jumped && can_reach_end[data.pos+1] {
+                    jumped = true;
+                    data.pos = data.pos + 1;
+                } else {
+                    data.pos = next_pos(dist, data.pos);
+                }
+            },
+        }
     }
-    result.to_string()
+    data.acc.to_string()
 }
